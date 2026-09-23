27f3bc04f2b6296a866a97b4d7673adcae335604
Theophilus Iornumbe <theoeasy247@gmail.com>
2026-09-23 17:18:31 +0100

    https://github.com/theoeasy247/Python-class

---
+++ lesson01.py
@@ -0,0 +1,78 @@
+"======================="
+
+"MY PYTHON INTRODUCTION"
+
+"======================="
+
+Name = "Theoeasy"
+
+Country = "Nigeria"
+
+Language = "Python"
+
+Goal = "Software Engineer"
+
+print(f"======================")
+
+print("MY PYTHON INTRODUCTION")
+
+print("======================")
+
+print(f"Name: {Name}")
+
+print(f"Country: {Country}")
+
+print(f"Language: {Language}")
+
+print(f"Goal: {Goal}")
+
+CHALLENGE = 1
+
+#import Receipt
+
+"=====RECEIPT====="
+
+Product = "Phone"
+
+Price = 500000
+
+Quantity = 2
+
+"=================="
+
+print("CHALLENGE", 1)
+
+print(f"=====RECEIPT=====")
+
+print(f"Rpoduct: {Product}")
+
+print(f"price: {500000}")
+
+print(f"Quantity: {2}")
+
+CHALLENGE = 2
+
+#Write the program that calculates:
+
+#25 + 15
+
+#25 - 15
+
+#25 * 15
+
+#25 / 15
+
+25 + 15
+25 - 15 
+25 * 15 
+25 / 15
+
+print("CHALLENGE", 2)
+
+print(25 + 15)
+
+print(25 - 15)
+
+print(25 * 15)
+
+print(25 / 15)
---
+++ lesson02.py
@@ -0,0 +1,60 @@
+name = "Theoeasy"
+age = 25 
+price = 500000.50
+is_student = True
+
+print(name)
+print(age)
+print(price)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(is_student))
+
+#LESSON2 EXERCISE
+student_name = "Theophilus"
+student_age = 30
+student_score = 90
+is_registered = True
+
+# Data Value
+
+print(student_name)
+print(student_age)
+print(student_score)
+print(is_registered)
+
+# Data Type
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_score))
+print(type(is_registered))
+
+# CHALLENGE
+#Create a small student information program 
+
+student_name = "Saater"  
+student_age = 29
+student_course = "Physics"
+student_score = 68
+is_registered = True
+
+print(f"name: {student_name}")
+print(f"age: {student_age}")
+print(f"course: {student_course}")
+print(f"score: {student_score}")
+print(f"registered: {is_registered}")
+
+person_name = "Aondofa"
+person_age = 40 
+account_balance = 100
+is_employed = False
+
+print(f"name: {person_name} | type: {type(person_name)}")
+print(f"age: {person_age} | type: {type(person_age)}")
+print(f"balance: {account_balance} | type: {type(account_balance)}")
+print(f"employed: {is_employed} | type: {type(is_employed)}")
+
